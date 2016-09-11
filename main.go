da3b50be4077fcef9ec1ddeb53b07134fcc8bfe3
Ryan Vennell <ryan.vennell@gmail.com>
2016-09-11 02:28:53 -0500

    Lots of changes to get the skeleton working.

--- main.go
+++ main.go
@@ -1,84 +1,52 @@
 package main
 
-//go:generate statik -src=./static/
+//go:generate statik -src=.\static
+//go generate go get github.com/jteeuwen/go-bindata/...
+//go:generate go-bindata -o settings.go -pkg main settings/...
 
 import (
-	"strconv"
+	"encoding/json"
+	"os"
 
 	"github.com/enlivengo/admin"
 	"github.com/enlivengo/database"
 	"github.com/enlivengo/enliven"
 	"github.com/enlivengo/enliven/config"
 	"github.com/enlivengo/enliven/middleware/session"
-	_ "github.com/enlivengo/example/statik"
+	_ "github.com/enlivengo/skeleton/statik"
 	"github.com/enlivengo/static"
 	"github.com/enlivengo/statik"
 	"github.com/enlivengo/user"
-    "./settings"
 )
 
-func rootHandler(ctx *enliven.Context) {
-	val := ctx.Session.Get("increments")
+func getConfig() config.Config {
+	environment := "development"
+	args := os.Args[1:]
+	if len(args) > 0 {
+		environment = args[0]
+	}
+	settingsBytes := MustAsset("settings/" + environment + ".json")
 
-	var value int
+	var enlivenConfig config.Config
 
-	if val == "" {
-		value = 1
-	} else {
-		value, _ = strconv.Atoi(val)
-		value++
+	if err := json.Unmarshal(settingsBytes, &enlivenConfig); err != nil {
+		panic(err)
 	}
 
-	newVal := strconv.Itoa(value)
-	ctx.Session.Set("increments", newVal)
-
-	ctx.Template("home")
+	return enlivenConfig
 }
 
-func getConfig() config.Config {
-    args := os.Args[1:]
-    if (len(args) == 0) {
-        return settings.
-    }
-}
-
-// Example/Test usage
 func main() {
-
 	ev := enliven.New(getConfig())
 
-	// Adding session management middleware
 	ev.AddMiddleware(session.NewMemoryStorageMiddleware())
-	//ev.AddMiddleware(session.NewFileStorageMiddleware())
-	//ev.AddMiddleware(session.NewMemoryStorageMiddleware())
-
-	// Serving static assets from the ./static/ folder at the /assets/ route
 	ev.AddApp(static.NewApp())
-
-	// The statik import at the top of this file sets up the data that will be used by the statik filesystem.
-	// Read Statik documentation
 	ev.AddApp(statik.NewApp())
-
-	// DATABASE The database app allows you to use....a database
 	ev.AddApp(database.NewApp())
-
-	// USER The user app manages the user model/login/session/middleware
 	ev.AddApp(user.NewApp())
-
-	// ADMIN The admin app manages the admin panel
 	ev.AddApp(admin.NewApp())
 
-	// Simple route handler
-	ev.AddRoute("/", rootHandler)
-
-	// This is a commented-out example of how you can override the existing header/footer templates.
-	// You will most likely want to do this as the built-in one is not meant for general consumption
-	// as it has embedded images(base64 encoded)/css/javascript.
-	/*
-		templates := ev.GetTemplates()
-		templates.Parse("{{define \"header\"}}OMG Becky did you see her butt?{{end}}")
-		templates.Parse("{{define \"footer\"}}OMG it's so big.{{end}}")
-	*/
+	ev.AddRoute("/", rootView)
 
 	ev.Run()
 }
--- views.go
+++ views.go
@@ -1,1 +1,7 @@
 package main
+
+import "github.com/enlivengo/enliven"
+
+func rootView(ctx *enliven.Context) {
+	ctx.Template("home")
+}
